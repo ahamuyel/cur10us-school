144ba4592620c0569ab8cabf7d7570537964246f
ahamuyel <alber701hamuyela@gmail.com>
2025-11-13 19:28:47 +0100

    just to save

--- cur10us_ui/app/dashboard/layout.tsx
+++ cur10us_ui/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function DasboardLayout({
   children,
@@ -8,7 +9,9 @@ export default function DasboardLayout({
     <div className="h-screen flex">
         {/* {children} */}
         {/* LEFT */}
-        <div className="w-1/6 md:w-[8%] lg:w-[18%] xl:w-[14%] bg-red-50">Left</div>
+        <div className="w-1/6 md:w-[8%]  lg:w-[18%] xl:w-[14%] bg-red-50">
+          <Link href="/">cu</Link>
+        </div>
         {/* RIGHT */}
         <div className="w-[89%] md:w-[92%] lg:w-[18%] xl:w-[92%] bg-blue-50">Right</div>
     </div>
