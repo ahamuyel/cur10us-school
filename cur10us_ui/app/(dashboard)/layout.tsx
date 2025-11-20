6c498731a3f4479dc667df9eae895e8335da6c0f
ahamuyel <alber701hamuyela@gmail.com>
2025-11-20 17:09:33 +0100

    nav bar done

---
+++ cur10us_ui/app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+
+export default function DasboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+        {/* {children} */}
+        {/* LEFT */}
+        <div className="w-1/6 md:w-[8%]  lg:w-[18%] xl:w-[14%] bg-red-50  p-4">
+          <Link href="/" className="flex items-center justify-content lg:justify-start gap-2">
+            <Image src="/logo.png" width={32} height={32} alt="logo"/>
+            <span className="hidden lg:inline text-md font-semibold">Cur10uSchool</span>
+          </Link>
+          <Menu />
+        </div>
+        {/* RIGHT */}
+        <div className="w-[89%] md:w-[92%] lg:w-[94%] xl:w-[92%] bg-blue-50">Right</div>
+    </div>
+  );
+}
