035c784f45b487dca586398f83aa668f1283a57f
Alber701H <alber701hamuyela@gmail.com>
2025-11-22 16:07:20 +0000

    Eventos

--- cur10us_ui/app/components/UserCard.tsx
+++ cur10us_ui/app/components/UserCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const UserCard = ({type}:{type:string}) =>{
     return (
-        <div className="rounded-2xl odd:bg-[#c3ebfa] even:bg-[#f6ed78ff] p-4 flex-1 min-w-[130px]">
+        <div className="rounded-2xl odd:bg-[#ab98dc] even:bg-[#f6ed78ff] p-4 flex-1 min-w-[130px]">
             <div className="flex justify-between items-center">
                 <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2025/26</span>
                 <Image src="/more.png" alt="" width={20} height={20} />
